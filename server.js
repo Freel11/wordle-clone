34f83f07d4d3e6c6a5d6c9a9b4ffcea6ab6a95bb
jade@itnt.co.za <jade@itnt.co.za>
2022-04-16 12:58:31 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+	res.render('index');
+})
+
+app.use('/static', express.static('static'))
+
+app.listen(5000)
