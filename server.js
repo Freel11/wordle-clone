ec0a4183b9d5cb4ef0db435126813a2f5da1bef5
jade@itnt.co.za <jade@itnt.co.za>
2022-04-16 13:47:10 +0200

    using process.env to define port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs')
-app.set('port', 3000)
+app.set('port', process.env.PORT || 3000)
 
 app.get('/', (req, res) => {
 	res.render('index');
